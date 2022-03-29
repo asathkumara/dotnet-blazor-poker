e53f9f6b2418927727f5bd81e77067f63e519e73
asathkumara <sathkumaraasel@gmail.com>
2022-03-28 17:13:16 -0700

    Added singleton instance for state container

--- DotnetBlazorPoker.App/Program.cs
+++ DotnetBlazorPoker.App/Program.cs
@@ -1,4 +1,5 @@
 using DotnetBlazorPoker.App;
+using DotnetBlazorPoker.Models;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -7,5 +8,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSingleton<GameStateManager>();
 
 await builder.Build().RunAsync();
