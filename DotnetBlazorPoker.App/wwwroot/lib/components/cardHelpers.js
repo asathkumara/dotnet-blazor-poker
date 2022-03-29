23eb7c7e975b3de230c090d8bb436a0fc6720f9e
asathkumara <sathkumaraasel@gmail.com>
2022-03-28 17:11:51 -0700

    Implemented JSInterop functions for card functionality

---
+++ DotnetBlazorPoker.App/wwwroot/lib/components/cardHelpers.js
@@ -0,0 +1,14 @@
+﻿const setBrightnessFilter = (cardElement, filterIntensity=0.8) => {
+    cardElement.style.filter = `brightness(${filterIntensity})`;
+};
+
+const resetFilter = (cardElement) => {
+    cardElement.style.filter = "none";
+};
+
+
+const setPointer = (cardElement) => {
+    cardElement.style.cursor = "pointer";
+}
+
+export { setBrightnessFilter, resetFilter, setPointer };
