689de98a8eab1f9bcc850aa5dd63de1c2b45c050
asathkumara <28933557+asathkumara@users.noreply.github.com>
2022-07-03 10:13:11 -0700

    Refactored components to use new js interop helpers

--- DotnetBlazorPoker.App/Program.cs
+++ DotnetBlazorPoker.App/Program.cs
@@ -2,6 +2,7 @@ using DotnetBlazorPoker.App;
 using DotnetBlazorPoker.Models;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.JSInterop;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,5 +10,10 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddSingleton<GameStateManager>();
+//await builder.Build().RunAsync();
 
-await builder.Build().RunAsync();
+var host = builder.Build();
+await ElementReferenceExtensions.Initialize(host.Services.GetRequiredService<IJSRuntime>());
+await Window.Initialize(host.Services.GetRequiredService<IJSRuntime>());
+
+await host.RunAsync();
